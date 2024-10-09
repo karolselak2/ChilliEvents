6976b5d9351ad631c4ccfb977b0ee2b70567ab10
Karol Selak <karol.selak@gmail.com>
2024-10-09 09:28:19 +0200

    feat: seeds and README

---
+++ README.md
@@ -0,0 +1,11 @@
+# ChilliEvents
+
+App needs Docker and Docker Compose to run. Basic commands:
+ - setup before first start: `bin/setup`
+ - databases creation: `sudo bin/run rails db:create && sudo bin/run rails db:create ENV=test`
+ - migration: `sudo bin/run rails db:migrate`
+ - seed: `sudo bin/run rails db:seed`
+ - starting the app on `localhost:3000`: `sudo docker compose up`
+ - running tests: `sudo bin/run bundle exed rspec`
+ - list endpoints: `sudo bin/run rails routes`
+
--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+User.destroy_all
+Event.destroy_all
+EventParticipant.destroy_all
+
+users = []
+5.times do |i|
+  users << User.create!(
+    first_name: "User#{i + 1}",
+    last_name: "Test#{i + 1}",
+    email: "user#{i + 1}@example.com"
+  )
+end
+
+events = []
+3.times do |i|
+  events << Event.create!(
+    name: "Event#{i + 1}",
+    organiser: users.sample,
+    start: Time.now + (i + 1).days,
+    end: Time.now + (i + 1).days + 2.hours,
+    participants_limit: rand(5..10)
+  )
+end
+
+events.each do |event|
+  rand(1..5).times do
+    EventParticipant.create!(
+      user: users.sample,
+      event: event
+    )
+  end
+end
+
+puts "Successfully seeded!"
